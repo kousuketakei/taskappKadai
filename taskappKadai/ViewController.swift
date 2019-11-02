8aa49be0934c257dddd6814badadefb19cc3ef5b
kousuketakei <tom20100320forever@gmail.com>
2019-11-02 18:44:21 +0900

    Initial Commit

---
+++ taskappKadai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskappKadai
+//
+//  Created by 武井康祐 on 2019/11/02.
+//  Copyright © 2019 KousukeTakei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
