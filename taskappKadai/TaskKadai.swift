b3409cb12aab3b2d20170caf7da29160822075cd
kousuketakei <tom20100320forever@gmail.com>
2019-11-04 10:25:08 +0900

    taskapp課題前の状態でローカル通知機能なし

---
+++ taskappKadai/TaskKadai.swift
@@ -0,0 +1,29 @@
+//
+//  TaskKadai.swift
+//  taskappKadai
+//
+//  Created by 武井康祐 on 2019/11/03.
+//  Copyright © 2019 KousukeTakei. All rights reserved.
+//
+
+import RealmSwift
+
+@objcMembers class Task: Object {
+    //管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    dynamic var title = ""
+    
+    
+    dynamic var contents = ""
+    
+    
+    dynamic var date = Date()
+    
+    /**
+    　idをプライマリーキーとして設定
+     */
+    override static func primaryKey() ->String? {
+        return "id"
+    }
+}
